b0ea69fcd7d65e585d4175fbd8364dbb661f112f
Eric Kodzi <kod24002@byui.edu>
2025-03-15 15:28:05 +0000

    Update swaggerUI URL

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: 'https://cse-341-project1-n4qt.onrender.com/',
                 description: 'Local server',
             },
         ],
