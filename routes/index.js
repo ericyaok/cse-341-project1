c263dd25e7c55c20bbe7b0983262054f6e193369
Eric Kodzi <kod24002@byui.edu>
2025-03-16 07:19:33 +0000

    remove swagger setup

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 
-router.use('/api-docs', require('./swagger.js'));
-
 router.get('/', (req, res) => { res.send("Hello World"); });
 
 router.use('/contacts', require('./contacts.js'));
